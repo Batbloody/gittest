fafdeeffa6dc516ef1cb6f271df776c7e77ce1c5
Tronsit <noteatomic678@gmail.com>
2020-10-20 17:44:05 +0700

    Feature

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -5,8 +5,12 @@ public class Gittest {
     
     public static void main(String[] args) {
        sayHello();
+       sayHi();
     }
     public static void sayHello() {
         System.out.println("Hello");
+    }
+    public static void sayHi(){
+        System.out.println("Hi");
     }
 }
